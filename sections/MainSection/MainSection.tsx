9ac9aef3076c2a4ccd7958141d802d824e46f4bf
sobbingtofu <kukubasak@naver.com>
2025-12-08 00:14:35 +0900

    Feat : 메인섹션 다국어 구현

--- sections/MainSection/MainSection.tsx
+++ sections/MainSection/MainSection.tsx
@@ -1,23 +1,43 @@
 import Image from "next/image";
 import {chironGoRoundTc, nanumMyeongjo} from "@/fonts/Fonts";
+import zustandStore from "@/store/zustandStore";
 
 function MainSection() {
+  const {selectedLanguage} = zustandStore();
   return (
     <section className="h-screen w-full flex items-center justify-center relative ">
       <div
         className="absolute z-20  w-[80%] h-[65%] flex flex-col items-center
         py-40 border-t border-t-gray-100 border-b border-b-gray-100"
       >
-        <h1 className={`${nanumMyeongjo.className} text-white text-8xl font-extrabold`}>미오앤나나</h1>
+        <h1 className={`${nanumMyeongjo.className} text-white text-8xl font-extrabold`}>
+          {selectedLanguage === "Korean" ? "미오앤나나" : "Cafe Mio & Nana"}
+        </h1>
 
-        <p className={`${nanumMyeongjo.className} text-white text-[20px] mt-7`}>따뜻한 시간이 시작되는 곳</p>
+        <p className={`${nanumMyeongjo.className} text-white text-[20px] mt-7`}>
+          {selectedLanguage === "Korean" ? "따뜻한 시간이 시작되는 곳" : "Where warmth meets you"}
+        </p>
 
         <div className="flex mt-30 items-center w-[60%] justify-between">
-          <p className={`w-[300px] text-right`}>{"영업시간 :  ~18시 (일요일 휴무)"}</p>
+          <p
+            className={`${chironGoRoundTc.className}
+            ${selectedLanguage === "Korean" ? "text-md" : "text-sm"}
+            w-[300px] text-right`}
+          >
+            {selectedLanguage === "Korean"
+              ? "영업시간 :  ~18시, 일요일 휴무"
+              : "Business Hours: ~6PM, Closed on Sundays"}
+          </p>
 
           <div className={`${chironGoRoundTc.className} w-[300px] flex flex-col items-start text-white gap-2`}>
-            <p className={``}>{"서울 중구 퇴계로28길 8-5"}</p>
-            <p className={`italic text-sm`}>{"(충무로역 4번출구, 명동역 3번출구 도보 3분)"}</p>
+            <p className={`${selectedLanguage === "Korean" ? "text-md" : "text-sm"}`}>
+              {selectedLanguage === "Korean" ? "서울 중구 퇴계로28길 8-5" : "8-5, Toegye-ro 28-gil, Jung-gu, Seoul"}
+            </p>
+            <p className={`italic ${selectedLanguage === "Korean" ? "text-sm" : "text-xs"} `}>
+              {selectedLanguage === "Korean"
+                ? "(충무로역 4번출구, 명동역 3번출구 도보 3분)"
+                : "(3 mins walk from Chungmuro Station Exit 4 & Myeongdong Station Exit 3)"}
+            </p>
           </div>
         </div>
       </div>
