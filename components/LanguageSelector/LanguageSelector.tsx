8072bbcd2a2976f91b0b014cb160f5819c5eb948
sobbingtofu <kukubasak@naver.com>
2025-12-07 23:22:29 +0900

    Style : 언어 선택영역 스타일 변경

--- components/LanguageSelector/LanguageSelector.tsx
+++ components/LanguageSelector/LanguageSelector.tsx
@@ -14,18 +14,18 @@ function LanguageSelector() {
   };
 
   return (
-    <div className="w-full fixed top-0 left-0 z-50 flex gap-4 bg-black/25 py-2 px-4 justify-end">
+    <div className="w-full fixed top-0 left-0 z-50 flex gap-4 py-4 px-12 justify-end bg-linear-to-b from-black/50 to-black/0">
       <button
         onClick={() => handleLanguageChange("Korean")}
-        className={`px-4 py-2 rounded transition-colors cursor-pointer
-          ${selectedLanguage === "Korean" ? " text-white font-semibold" : "text-gray-700 hover:text-gray-200"}
+        className={`transition-colors cursor-pointer
+          ${selectedLanguage === "Korean" ? " text-white font-semibold" : "text-gray-400 hover:text-gray-200"}
           `}
       >
         한국어
       </button>
       <button
         onClick={() => handleLanguageChange("English")}
-        className={`px-4 py-2 rounded transition-colors cursor-pointer
+        className={`transition-colors cursor-pointer
           ${selectedLanguage === "English" ? " text-white font-semibold" : "text-gray-400 hover:text-gray-200"}
           `}
       >
