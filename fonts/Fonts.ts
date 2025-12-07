469aa00a1a315e95c9fa4a9b9bcbc164912f3e90
sobbingtofu <kukubasak@naver.com>
2025-12-08 00:07:26 +0900

    Style : 메인섹션 스타일 구현

--- fonts/Fonts.ts
+++ fonts/Fonts.ts
@@ -1,4 +1,4 @@
-import {Nanum_Myeongjo, Roboto} from "next/font/google";
+import {Chiron_GoRound_TC, Nanum_Gothic, Nanum_Myeongjo, Roboto} from "next/font/google";
 
 export const roboto = Roboto({
   weight: ["400", "700", "900"],
@@ -8,6 +8,18 @@ export const roboto = Roboto({
 
 export const nanumMyeongjo = Nanum_Myeongjo({
   weight: ["400", "700", "800"],
+  subsets: ["latin"],
+  display: "swap",
+});
+
+export const nanumGothic = Nanum_Gothic({
+  weight: ["400", "700", "800"],
+  subsets: ["latin"],
+  display: "swap",
+});
+
+export const chironGoRoundTc = Chiron_GoRound_TC({
+  weight: ["400", "700", "900"],
   subsets: ["latin"],
   display: "swap",
 });
--- sections/MainSection/MainSection.tsx
+++ sections/MainSection/MainSection.tsx
@@ -1,19 +1,29 @@
 import Image from "next/image";
-import {nanumMyeongjo} from "@/fonts/Fonts";
+import {chironGoRoundTc, nanumMyeongjo} from "@/fonts/Fonts";
 
 function MainSection() {
   return (
     <section className="h-screen w-full flex items-center justify-center relative ">
       <div
-        className="border-4 border-red-500 absolute z-20  w-[80%] h-[70%] flex flex-col items-center
-        py-40"
+        className="absolute z-20  w-[80%] h-[65%] flex flex-col items-center
+        py-40 border-t border-t-gray-100 border-b border-b-gray-100"
       >
-        <h1 className={`${nanumMyeongjo.className} text-white text-7xl font-bold`}>미오앤나나</h1>
-        <p className={`${nanumMyeongjo.className} text-white text-xl mt-4`}>따뜻한 시간이 시작되는 곳</p>
+        <h1 className={`${nanumMyeongjo.className} text-white text-8xl font-extrabold`}>미오앤나나</h1>
+
+        <p className={`${nanumMyeongjo.className} text-white text-[20px] mt-7`}>따뜻한 시간이 시작되는 곳</p>
+
+        <div className="flex mt-30 items-center w-[60%] justify-between">
+          <p className={`w-[300px] text-right`}>{"영업시간 :  ~18시 (일요일 휴무)"}</p>
+
+          <div className={`${chironGoRoundTc.className} w-[300px] flex flex-col items-start text-white gap-2`}>
+            <p className={``}>{"서울 중구 퇴계로28길 8-5"}</p>
+            <p className={`italic text-sm`}>{"(충무로역 4번출구, 명동역 3번출구 도보 3분)"}</p>
+          </div>
+        </div>
       </div>
       <div className="bg-red-300 h-dvh w-dvw relative shrink-0">
         <Image src="/cafe-images/13.jpg" alt="Main Image" fill className="object-cover" priority />
-        <div className="absolute inset-0 bg-linear-to-b from-black/30 to-black/90 z-10" />
+        <div className="absolute inset-0 bg-linear-to-b from-black/50 to-black/90 z-10 backdrop-blur-xs" />
       </div>
     </section>
   );
