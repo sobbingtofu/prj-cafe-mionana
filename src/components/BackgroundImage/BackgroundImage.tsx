6ec6682fd8c987c34964dbd45d701e4de866e7f6
sobbingtofu <kukubasak@naver.com>
2025-12-23 03:05:56 +0900

    Fix : 동적 스타일링 적용된 tailwind css 변경

--- src/components/BackgroundImage/BackgroundImage.tsx
+++ src/components/BackgroundImage/BackgroundImage.tsx
@@ -7,11 +7,11 @@ interface BackgroundImageProps {
 }
 
 function BackgroundImage({imgUrl, backdrop, backdropOpacity = 0.5}: BackgroundImageProps) {
-  const backdropClassNameText = `w-full absolute inset-0 bg-[rgba(0,0,0,${backdropOpacity})] z-10`;
-
   return (
     <div className="min-h-[530px] h-screen w-screen relative shrink-0">
-      {backdrop && <div className={backdropClassNameText} />}
+      {backdrop && (
+        <div className="w-full absolute inset-0 z-10" style={{backgroundColor: `rgba(0, 0, 0, ${backdropOpacity})`}} />
+      )}
       <Image src={imgUrl} alt="Main Image" fill className="object-cover" priority />
     </div>
   );
